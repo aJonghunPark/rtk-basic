4c4da4ac66e2e537f27ceb7408b7b2c8afd301c9
Jonghun Park - 朴 鍾勲 <devtopia@icloud.com>
2023-06-01 18:13:42 +0900

    Improve the runtime when testing

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,20 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jest-environment-jsdom",
-  testPathIgnorePatterns: ["<rootDir>/node_modules"],
+  testMatch: [
+    "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
+    "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}",
+  ],
+  testPathIgnorePatterns: ["<rootDir>/dist", "<rootDir>/node_modules"],
+  modulePathIgnorePatterns: [
+    "<rootDir>/(build|docs|node_modules|scripts|dist)",
+  ],
   moduleNameMapper: {
-    "\\.(css)$": "<rootDir>/node_modules/jest-css-modules",
+    "^.+\\.(css|sass|scss)$": "identity-obj-proxy",
   },
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.svg$": "<rootDir>/svgTransform.js",
+    "^.+\\.(js|jsx|ts|tsx)$": "esbuild-jest",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/fileTransformer.js",
   },
 };
